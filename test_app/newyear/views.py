ef77eb44633420a53d6f17623857a88197069c94
claeryc <reeko.it@gmail.com>
2025-07-25 21:25:33 +0800

    change view files

--- test_app/newyear/views.py
+++ test_app/newyear/views.py
@@ -5,5 +5,6 @@ import datetime
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
-        "newyear": now.month == 1 and now.day == 1
+        "newyear": now.month == 1 and now.day == 1,
+        "now": now.date
     })

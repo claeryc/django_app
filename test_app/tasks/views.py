46ce79213ad7ffb750fc219db62fa888e3385fc2
claeryc <reeko.it@gmail.com>
2025-07-27 16:27:22 +0800

    initialize tasks app

---
+++ test_app/tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+tasks = ["task-1", "task-2", "task-3"]
+
+# Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
--- test_app/test_app/urls.py
+++ test_app/test_app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('greet/', include("greet.urls")),
-    path('newyear/', include("newyear.urls"))
+    path('newyear/', include("newyear.urls")),
+    path('tasks/', include("tasks.urls"))
 ]

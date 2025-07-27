06c5aa542fd6001477a733fe3a6cbe540fe5e11c
claeryc <reeko.it@gmail.com>
2025-07-27 17:15:02 +0800

    client and serverside form validation

--- test_app/tasks/views.py
+++ test_app/tasks/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django import forms
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "New Task")
+    priority = forms.IntegerField(label = "Priority", min_value=1, max_value=5)
 
 tasks = ["task-1", "task-2", "task-3"]
 
@@ -6,4 +11,18 @@ tasks = ["task-1", "task-2", "task-3"]
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.thml", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
     })
